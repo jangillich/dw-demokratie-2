9b99f633ad3c795e7fadf3f66763a2d4ad5d1547
Jan Gillich <gillichjan@gmail.com>
2016-06-21 19:15:55 +0200

    android project

---
+++ app/src/androidTest/java/de/lmu/dwII2016/demo2/festivalfrunangepasstekunst/ApplicationTest.java
@@ -0,0 +1,13 @@
+package de.lmu.dwII2016.demo2.festivalfrunangepasstekunst;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
